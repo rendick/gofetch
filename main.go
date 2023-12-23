84ee676a53e1f90f43c6d2767589a657ff4529f9
rendick <rendick123@proton.me>
2023-12-23 16:49:59 +0200

    display, terminal, manager

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"gofetch/cmd/display"
 	"gofetch/cmd/hostname"
 	"gofetch/cmd/kernel"
 	"gofetch/cmd/manager"
+	"gofetch/cmd/terminal"
 	"gofetch/cmd/uptime"
 	"gofetch/cmd/user"
 	"gofetch/cmd/weather"
@@ -14,6 +16,8 @@ func main() {
 	hostname.Hostname()
 	uptime.Uptime()
 	kernel.Kernel()
+	terminal.Terminal()
 	weather.Weather()
 	manager.Manager()
+	display.Display()
 }
