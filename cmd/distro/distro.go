a77d57801e642a40ce09f6917aa5f340e25753e0
richard <kostianaddur@gmail.com>
2023-12-24 19:49:01 +0200

    readme

--- cmd/distro/distro.go
+++ cmd/distro/distro.go
@@ -15,22 +15,20 @@ var (
 func Distro() {
 	check_distro, err_distro := exec.Command("uname", "-o").Output()
 	if err_distro != nil {
-		os.Exit(1)
+		os.Exit(0)
 	}
 
 	if strings.TrimSpace(string(check_distro)) == "GNU/Linux" {
 		distro_linux, err := exec.Command("lsb_release", "-si").Output()
 		if err != nil {
-			fmt.Println("Error! Distro")
+			os.Exit(0)
 		}
 		fmt.Printf(Red+"Distribution: "+Reset+"%s", distro_linux)
 	} else if strings.TrimSpace(string(check_distro)) == "Android" {
 		distro_android, err_android := exec.Command("uname", "-o").Output()
 		if err_android != nil {
-			fmt.Println("Error! Android")
-			os.Exit(2)
+			os.Exit(0)
 		}
 		fmt.Printf(Red+"Distribution: "+Reset+"%s", distro_android)
 	}
-
 }
