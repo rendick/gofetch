e48c03473900a7c92b4762a3c36cabce4f548557
richard <kostianaddur@gmail.com>
2023-12-24 20:10:52 +0200

    cpu.go

--- cmd/cpu/cpu.go
+++ cmd/cpu/cpu.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 var (
@@ -12,10 +13,22 @@ var (
 )
 
 func CPU() {
-	cpu, err := exec.Command("sh", "-c", "lscpu | grep 'Model name' | cut -f 2 -d ':' | awk '{$1=$1}1'").Output()
-	if err != nil {
+	check_distro, err_distro := exec.Command("uname", "-o").Output()
+	if err_distro != nil {
 		os.Exit(0)
-	} else {
-		fmt.Printf(Red+"CPU:"+Reset+"%s", cpu)
+	} else if strings.TrimSpace(string(check_distro)) == "GNU/Linux" {
+		cpu_linux, err_linux := exec.Command("sh", "-c", "lscpu | grep 'Model name' | cut -f 2 -d ':' | awk '{$1=$1}1'").Output()
+		if err_linux != nil {
+			os.Exit(0)
+		} else {
+			fmt.Printf(Red+"CPU: "+Reset+"%s", cpu_linux)
+		}
+	} else if strings.TrimSpace(string(check_distro)) == "Android" {
+		cpu_android, err_android := exec.Command("sh", "-c", "lscpu | grep 'Model name' | cut -f 2 -d ':' | awk '{$1=$1}1'").Output()
+		if err_android != nil {
+			os.Exit(0)
+		} else {
+			fmt.Printf(Red+"CPU: "+Reset+"%s", cpu_android)
+		}
 	}
 }
