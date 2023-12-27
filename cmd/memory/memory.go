b86dacc84f7a483432d97f329369ef264c206e8a
rendick <rendick123@proton.me>
2023-12-27 16:57:05 +0200

    memory, display server protocol

--- cmd/memory/memory.go
+++ cmd/memory/memory.go
@@ -18,7 +18,7 @@ func Memory() {
 		os.Exit(0)
 	}
 
-	total_ram := int64(in.Totalram) / 1000
-	free_ram := int64(in.Freeram) / 1000
-	fmt.Printf(Red+"Memory: "+Reset+"%d MB / %d MB\n", free_ram / 1000, total_ram / 1000)
+	total_ram := int64(in.Totalram) / 1024
+	free_ram := int64(in.Freeram) / 1024
+	fmt.Printf(Red+"Memory: "+Reset+"%d MB / %d MB\n", free_ram / 1024, total_ram / 1024)
 }
---
+++ cmd/server/server.go
@@ -0,0 +1,36 @@
+package server
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+var (
+	Reset = "\033[0m"
+	Red   = "\033[31m"
+)
+
+func Server() {
+	check_distro, err_distro := exec.Command("uname", "-o").Output()
+	if err_distro != nil {
+		os.Exit(0)
+	}
+
+	if strings.TrimSpace(string(check_distro)) == "GNU/Linux" {
+		server_linux, err_linux := exec.Command("sh", "-c", "echo $XDG_SESSION_TYPE").Output()
+		if err_linux != nil {
+			os.Exit(0)
+		} else {
+			fmt.Printf(Red+"Server: "+Reset+"%s", server_linux)
+		}
+	} else if strings.TrimSpace(string(check_distro)) == "Android" {
+		server_android, err_android := exec.Command("sh", "-c", "echo binder").Output()
+		if err_android != nil {
+			os.Exit(0)
+		} else {
+			fmt.Printf(Red+"Server: "+Reset+"%s", server_android)
+		}
+	}
+}
