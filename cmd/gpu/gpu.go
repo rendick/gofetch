821854bcf56b5199e0ad8e78deec5e965938ac62
rendick <kostianaddur@gmail.com>
2023-12-27 17:46:59 +0200

    gpu

---
+++ cmd/gpu/gpu.go
@@ -0,0 +1,30 @@
+package gpu
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+var (
+	Reset = "\033[0m"
+	Red   = "\033[31m"
+)
+
+func GPU() {
+	check_distro, err_distro := exec.Command("uname", "-o").Output()
+	if err_distro != nil {
+		os.Exit()
+	}
+	if strings.TrimSpace(string(check_distro)) == "GNU/Linux" {
+		gpu_linux, err_linux := exec.Command("sh", "-c", "lspci | grep VGA").Output()
+		if err_linux != nil {
+			os.Exit(0)
+		} else {
+			fmt.Printf(Red+"GPU: "+Reset+"%s", gpu_linux)
+		}
+	} else if strings.TrimSpace(string(check_distro)) == "Android" {
+		os.Exit(0)
+	}
+}
