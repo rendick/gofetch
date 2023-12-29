339551015f25446b5d9f49b330c195728a37618d
rendick <rendick123@proton.me>
2023-12-29 13:15:23 +0100

    gpu

--- cmd/gpu/gpu.go
+++ cmd/gpu/gpu.go
@@ -16,13 +16,14 @@ func GPU() {
 	check_distro, err_distro := exec.Command("uname", "-o").Output()
 	if err_distro != nil {
 		os.Exit(0)
-	}
-	if strings.TrimSpace(string(check_distro)) == "GNU/Linux" {
+	} else if strings.TrimSpace(string(check_distro)) == "GNU/Linux" {
 		gpu_linux, err_linux := exec.Command("sh", "-c", "lspci | grep VGA").Output()
 		if err_linux != nil {
 			os.Exit(0)
 		} else {
 			fmt.Printf(Red+"GPU: "+Reset+"%s", gpu_linux)
 		}
+	} else if strings.TrimSpace(string(check_distro)) == "Android" {
+		os.Exit(0)
 	}
 }
