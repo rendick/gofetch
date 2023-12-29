0e689298d6fe7f0c63d5b65ebc1d916eb7750edd
rendick <rendick123@proton.me>
2023-12-29 14:45:46 +0100

    hostname android

--- cmd/hostname/hostname.go
+++ cmd/hostname/hostname.go
@@ -28,7 +28,7 @@ func Hostname() {
 		if err_android != nil {
 			os.Exit(0)
 		} else {
-			fmt.Printf("Hostname: %s", hostname_android)
+			fmt.Printf(Red+"Hostname: "+Reset+"%s", hostname_android)
 		}
 	}
 }
