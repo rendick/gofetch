861b5ec77edeb3efb7ad8356f8a7afa7903bc280
rendick <rendick123@proton.me>
2023-12-29 14:24:16 +0100

    kernel android support

--- cmd/kernel/kernel.go
+++ cmd/kernel/kernel.go
@@ -2,7 +2,9 @@ package kernel
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
+	"strings"
 )
 
 var (
@@ -11,10 +13,23 @@ var (
 )
 
 func Kernel() {
-	kernel, err := exec.Command("uname", "--kernel-release").Output()
-	if err != nil {
-		fmt.Println("Error! kernel")
-	}
+	check_distro, err_distro := exec.Command("uname", "-o").Output()
+	if err_distro != nil {
+		os.Exit(0)
+	} else if strings.TrimSpace(string(check_distro)) == "GNU/Linux" {
+		kernel_linux, err_linux := exec.Command("uname", "--kernel-release").Output()
+		if err_linux != nil {
+			os.Exit(0)
+		} else {
 
-	fmt.Printf(Red+"Kernel: "+Reset+"%s", kernel)
+			fmt.Printf(Red+"Kernel: "+Reset+"%s", kernel_linux)
+		}
+	} else if strings.TrimSpace(string(check_distro)) == "Android" {
+		kernel_android, err_android := exec.Command("uname", "--kernel-release").Output()
+		if err_android != nil {
+			os.Exit(0)
+		} else {
+			fmt.Printf(Red+"Kernel: "+Reset+"%s", kernel_android)
+		}
+	}
 }
