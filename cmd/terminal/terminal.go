10175b2c9cd2f2945e8b83f77c4c7edb29d735ff
rendick <kostianaddur@gmail.com>
2023-12-29 13:15:23 +0100

    gpu

--- cmd/terminal/terminal.go
+++ cmd/terminal/terminal.go
@@ -2,7 +2,9 @@ package terminal
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
+	"strings"
 )
 
 var (
@@ -11,9 +13,23 @@ var (
 )
 
 func Terminal() {
-	terminal, err := exec.Command("sh", "-c", "echo $TERM").Output()
-	if err != nil {
-		fmt.Println("Error! Terminal")
+	check_distro, err_distro := exec.Command("uname", "-o").Output()
+	if err_distro != nil {
+		os.Exit(0)
+	} else if strings.TrimSpace(string(check_distro)) == "GNU/Linux" {
+		terminal_linux, err_linux := exec.Command("sh", "-c", "echo $TERM").Output()
+		if err_linux != nil {
+			fmt.Println("Error! Terminal")
+		} else {
+			fmt.Printf(Red+"Terminal: "+Reset+"%s", terminal_linux)
+		}
+	} else if strings.TrimSpace(string(check_distro)) == "Android" {
+		terminal_android, err_android := exec.Command("sh", "-c", "echo $TERM").Output()
+		if err_android != nil {
+			fmt.Println("Error! Terminal")
+		} else {
+			fmt.Printf(Red+"Terminal: "+Reset+"%s", terminal_android)
+		}
 	}
-	fmt.Printf(Red+"Terminal: "+Reset+"%s", terminal)
+
 }
