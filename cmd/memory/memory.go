2b0ddcf4b5605ba1b94e8566b7993cfed728c3ba
richard <kostianaddur@gmail.com>
2024-01-01 14:17:17 +0200

    Memory, Android support

--- cmd/memory/memory.go
+++ cmd/memory/memory.go
@@ -16,26 +16,55 @@ var (
 var MemoryInfo string
 
 func Memory() {
-	avail_memory_linux, err_avail := exec.Command("sh", "-c", "cat /proc/meminfo | grep 'MemAvailable:' | awk '{print $2}'").Output()
-	if err_avail != nil {
+	check_distro, err_distro := exec.Command("uname", "-o").Output()
+	if err_distro != nil {
 		os.Exit(0)
-	}
-	total_memory_linux, err_total := exec.Command("sh", "-c", "cat /proc/meminfo | grep 'MemTotal:' | awk '{print $2}'").Output()
-	if err_total != nil {
-		os.Exit(0)
-	}
+	} else if strings.TrimSpace(string(check_distro)) == "GNU/Linux" {
 
-	avail_convert := strings.TrimSpace(string(avail_memory_linux))
-	avail, err_convert_avail := strconv.Atoi(avail_convert)
-	if err_convert_avail != nil {
-		panic(err_avail)
-	}
+		avail_memory_linux, err_avail := exec.Command("sh", "-c", "cat /proc/meminfo | grep 'MemAvailable:' | awk '{print $2}'").Output()
+		if err_avail != nil {
+			os.Exit(0)
+		}
+		total_memory_linux, err_total := exec.Command("sh", "-c", "cat /proc/meminfo | grep 'MemTotal:' | awk '{print $2}'").Output()
+		if err_total != nil {
+			os.Exit(0)
+		}
 
-	total_convert := strings.TrimSpace(string(total_memory_linux))
-	total, err_convert_total := strconv.Atoi(total_convert)
-	if err_convert_total != nil {
-		panic(err_total)
-	}
+		avail_convert := strings.TrimSpace(string(avail_memory_linux))
+		avail, err_convert_avail := strconv.Atoi(avail_convert)
+		if err_convert_avail != nil {
+			panic(err_avail)
+		}
 
-	MemoryInfo = fmt.Sprintf(Red+"Memory: "+Reset+"%d MB / %d MB", (total-avail)/1024, total/1024)
+		total_convert := strings.TrimSpace(string(total_memory_linux))
+		total, err_convert_total := strconv.Atoi(total_convert)
+		if err_convert_total != nil {
+			panic(err_total)
+		}
+
+		MemoryInfo = fmt.Sprintf(Red+"Memory: "+Reset+"%d MB / %d MB", (total-avail)/1024, total/1024)
+	} else if strings.TrimSpace(string(check_distro)) == "Android" {
+		avail_memory_android, err_avail_android := exec.Command("sh", "-c", "cat /proc/meminfo | grep 'MemAvailable:' | awk '{print $2}'").Output()
+		if err_avail_android != nil {
+			os.Exit(0)
+		}
+		total_memory_android, err_total_android := exec.Command("sh", "-c", "cat /proc/meminfo | grep 'MemTotal:' | awk '{print $2}'").Output()
+		if err_total_android != nil {
+			os.Exit(0)
+		}
+
+		avail_convert_android := strings.TrimSpace(string(avail_memory_android))
+		avail_android, err_convert_avail_android := strconv.Atoi(avail_convert_android)
+		if err_convert_avail_android != nil {
+			panic(err_avail_android)
+		}
+
+		total_convert_android := strings.TrimSpace(string(total_memory_android))
+		total_android, err_convert_total := strconv.Atoi(total_convert_android)
+		if err_convert_total != nil {
+			panic(err_total_android)
+		}
+		MemoryInfo = fmt.Sprintf(Red+"Memory: "+Reset+"%d MB / %d MB", (total_android-avail_android)/1024, total_android/1024)
+
+	}
 }
